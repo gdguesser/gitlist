ba852f744edf47092214cc5900e8df6072845d12
gdguesser <gdguesser@gmail.com>
2022-10-20 23:47:41 +0100

    Improving code and returning all public repositories of a user

--- main.go
+++ main.go
@@ -4,49 +4,59 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
 type Repository struct {
-	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	Language string `jsonL:"language"`
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Language    string `jsonL:"language"`
+	CreatedAt   string `jsonL:"created_at"`
+	UpdatedAt   string `jsonL:"updated_at"`
 }
 
 func main() {
-	fetchGithubRepositoriesInformation()
+	repositories, err := fetchGithubRepositoriesInformation()
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, v := range repositories {
+		fmt.Printf("Id: %d Nome: %s Linguagem: %s\n", v.Id, v.Name, v.Language)
+	}
+
 }
 
-func fetchGithubRepositoriesInformation() error {
+func fetchGithubRepositoriesInformation() ([]Repository, error) {
 	token := os.Getenv("GITHUB_TOKEN")
 	bearer := "Bearer " + token
 
 	req, err := http.NewRequest(http.MethodGet, "https://api.github.com/users/gdguesser/repos", nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	req.Header.Set("Authorization", bearer)
 	req.Header.Set("Content-Type", "application/json")
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer res.Body.Close()
 
 	bytes, err := io.ReadAll(res.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	var response []Repository
-	err = json.Unmarshal([]byte(bytes), &response)
+	var repositories []Repository
+	err = json.Unmarshal([]byte(bytes), &repositories)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	fmt.Println(response)
-
-	return nil
+	return repositories, nil
 }
