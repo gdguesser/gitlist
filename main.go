853b280409ebfae133e885f40b4e408dd7061313
gdguesser <gdguesser@gmail.com>
2022-10-21 00:09:45 +0100

    Fetching data and rendering a chart

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
 )
 
 type Repository struct {
@@ -21,15 +24,40 @@ type Repository struct {
 func main() {
 	args := os.Args[1]
 
+	// create a new bar instance
+	bar := charts.NewBar()
+	// set some global options like Title/Legend/ToolTip or anything else
+	bar.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+		Title:    "Language by " + args + " repositories",
+		Subtitle: "Usage by language",
+	}))
+
 	repositories, err := fetchGithubRepositoriesInformation(args)
 	if err != nil {
 		log.Println(err)
 	}
 
+	var goLangNum int
+	var javaLangNum int
+
 	for _, v := range repositories {
+		if v.Language == "Go" {
+			goLangNum += 1
+		} else if v.Language == "Java" {
+			javaLangNum += 1
+		}
 		fmt.Printf("Id: %d Nome: %s Linguagem: %s\n", v.Id, v.Name, v.Language)
 	}
 
+	fmt.Println("goLangNum: ", goLangNum)
+	fmt.Println("javaLangNum: ", javaLangNum)
+
+	// Put data into instance
+	bar.SetXAxis([]string{"Go", "Ts", "Java", "Thu", "Fri", "Sat", "Sun"}).
+		AddSeries("Golang", generateBarItems(goLangNum)).
+		AddSeries("Java", generateBarItems(javaLangNum))
+	f, _ := os.Create("bar.html")
+	bar.Render(f)
 }
 
 func fetchGithubRepositoriesInformation(user string) ([]Repository, error) {
@@ -61,4 +89,13 @@ func fetchGithubRepositoriesInformation(user string) ([]Repository, error) {
 	}
 
 	return repositories, nil
+}
+
+// generate random data for bar chart
+func generateBarItems(num int) []opts.BarData {
+	items := make([]opts.BarData, 0)
+	for i := 0; i < 7; i++ {
+		items = append(items, opts.BarData{Value: num})
+	}
+	return items
 }
